8c21fa8c9b6e277e50cf0ba79617253d8f7c8e1d
Raunaq98 <raunaq.bhalla98@gmail.com>
2020-07-08 01:00:08 +0530

    normal distribution

---
+++ linear regression.R
@@ -0,0 +1,24 @@
+#Consider a linear model :
+
+
+#  Y = B0 + B1.X + e
+#  Y is dependent variable
+#  X is independent variable in natural distribution with sd=1 ie. X = N(0,1^2)
+#  B0 is intercept constant of value 0.5
+#  B1 is slope constant of value 2
+#  e is noise in the form of natural distrubution with sd=2 ie. e = N(0,2^2)
+
+
+set.seed(1)   #when working with rnorm, always set seed
+x <- rnorm(100,0,1)
+e <- rnorm(100,0,2)
+y<- 0.5 + x*2 + e
+
+plot(x,y)
+
+summary(y)
+#   Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
+#-5.4816 -1.1493  0.7582  0.6422  2.3404  6.1534 
+
+
+  
